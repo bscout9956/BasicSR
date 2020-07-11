304a0dd9100986789394774951248de78d325406
BlackScout <bs.blackscout.bs@gmail.com>
2020-07-11 01:13:48 -0300

    Fix indentation issues
    Those could break the code

--- codes/models/SRRaGAN_model.py
+++ codes/models/SRRaGAN_model.py
@@ -447,67 +447,67 @@ class SRRaGANModel(BaseModel):
             else:
                 l_g_total.backward()
 
-        if self.cri_gan:
-            # D
-            for p in self.netD.parameters():
-                p.requires_grad = True
+            if self.cri_gan:
+                # D
+                for p in self.netD.parameters():
+                    p.requires_grad = True
 
-            l_d_total = 0
-            with autocast():
-                if self.opt['train']['gan_type'] == 'ciplab':
-                    e_S, d_S, _, _ = self.netD( self.fake_H.detach() )
-                    e_H, d_H, _, _ = self.netD( self.var_ref )
+                l_d_total = 0
+                with autocast():
+                    if self.opt['train']['gan_type'] == 'ciplab':
+                        e_S, d_S, _, _ = self.netD( self.fake_H.detach() )
+                        e_H, d_H, _, _ = self.netD( self.var_ref )
 
-                    # D Loss, for encoder end and decoder end
-                    l_d_enc_S = torch.nn.ReLU()(1.0 + e_S).mean() / bm
-                    l_d_enc_H = torch.nn.ReLU()(1.0 - e_H).mean() / bm
+                        # D Loss, for encoder end and decoder end
+                        l_d_enc_S = torch.nn.ReLU()(1.0 + e_S).mean() / bm
+                        l_d_enc_H = torch.nn.ReLU()(1.0 - e_H).mean() / bm
 
-                    l_d_dec_S = torch.nn.ReLU()(1.0 + d_S).mean() / bm
-                    l_d_dec_H = torch.nn.ReLU()(1.0 - d_H).mean() / bm
-                    self.log_dict['l_d_enc_H'] += l_d_enc_H.item()
-                    self.log_dict['l_d_dec_H'] += l_d_dec_H.item()
-                    self.log_dict['l_d_enc_S'] += l_d_enc_S.item()
-                    self.log_dict['l_d_dec_S'] += l_d_dec_S.item()
+                        l_d_dec_S = torch.nn.ReLU()(1.0 + d_S).mean() / bm
+                        l_d_dec_H = torch.nn.ReLU()(1.0 - d_H).mean() / bm
+                        self.log_dict['l_d_enc_H'] += l_d_enc_H.item()
+                        self.log_dict['l_d_dec_H'] += l_d_dec_H.item()
+                        self.log_dict['l_d_enc_S'] += l_d_enc_S.item()
+                        self.log_dict['l_d_dec_S'] += l_d_dec_S.item()
 
-                    l_d_total = l_d_enc_H + l_d_dec_H + l_d_enc_S + l_d_dec_S
-                else:
-                    if self.use_frequency_separation:
-                        pred_d_real = self.netD(self.filter_high(self.var_ref))
-                        pred_d_fake = self.netD(self.filter_high(self.fake_H.detach())) # detach to avoid BP to G
+                        l_d_total = l_d_enc_H + l_d_dec_H + l_d_enc_S + l_d_dec_S
                     else:
-                        pred_d_real = self.netD(self.var_ref)
-                        pred_d_fake = self.netD(self.fake_H.detach())  # detach to avoid BP to G
-                    l_d_real = self.cri_gan(pred_d_real - torch.mean(pred_d_fake), True) / bm
-                    l_d_fake = self.cri_gan(pred_d_fake - torch.mean(pred_d_real), False) / bm
-                    self.log_dict['l_d_real'] += l_d_real.item()
-                    self.log_dict['l_d_fake'] += l_d_fake.item()
+                        if self.use_frequency_separation:
+                            pred_d_real = self.netD(self.filter_high(self.var_ref))
+                            pred_d_fake = self.netD(self.filter_high(self.fake_H.detach())) # detach to avoid BP to G
+                        else:
+                            pred_d_real = self.netD(self.var_ref)
+                            pred_d_fake = self.netD(self.fake_H.detach())  # detach to avoid BP to G
+                        l_d_real = self.cri_gan(pred_d_real - torch.mean(pred_d_fake), True) / bm
+                        l_d_fake = self.cri_gan(pred_d_fake - torch.mean(pred_d_real), False) / bm
+                        self.log_dict['l_d_real'] += l_d_real.item()
+                        self.log_dict['l_d_fake'] += l_d_fake.item()
 
-                    l_d_total = (l_d_real + l_d_fake) / 2
+                        l_d_total = (l_d_real + l_d_fake) / 2
 
-                    if self.opt['train']['gan_type'] == 'wgan-gp':
-                        batch_size = self.var_ref.size(0)
-                        if self.random_pt.size(0) != batch_size:
-                            self.random_pt.resize_(batch_size, 1, 1, 1)
-                        self.random_pt.uniform_()  # Draw random interpolation points
-                        interp = self.random_pt * self.fake_H.detach() + (1 - self.random_pt) * self.var_ref
-                        interp.requires_grad = True
-                        interp_crit = self.netD(interp)
-                        l_d_gp = self.l_gp_w * self.cri_gp(interp, interp_crit) / bm
-                        l_d_total += l_d_gp
-                        self.log_dict['l_d_gp'] += l_d_gp.item()
-            
-            if use_amp:
-                self.scaler.scale(l_d_total).backward()
-            else:
-                l_d_total.backward()
+                        if self.opt['train']['gan_type'] == 'wgan-gp':
+                            batch_size = self.var_ref.size(0)
+                            if self.random_pt.size(0) != batch_size:
+                                self.random_pt.resize_(batch_size, 1, 1, 1)
+                            self.random_pt.uniform_()  # Draw random interpolation points
+                            interp = self.random_pt * self.fake_H.detach() + (1 - self.random_pt) * self.var_ref
+                            interp.requires_grad = True
+                            interp_crit = self.netD(interp)
+                            l_d_gp = self.l_gp_w * self.cri_gp(interp, interp_crit) / bm
+                            l_d_total += l_d_gp
+                            self.log_dict['l_d_gp'] += l_d_gp.item()
+                
+                    if use_amp:
+                        self.scaler.scale(l_d_total).backward()
+                    else:
+                        l_d_total.backward()
 
+        
+                    if self.opt['train']['gan_type'] == 'ciplab':
+                        pass # TODO?
+                    else:
+                        self.log_dict['D_real'] += torch.mean(pred_d_real.detach()).item() / bm
+                        self.log_dict['D_fake'] += torch.mean(pred_d_fake.detach()).item() / bm
         # D outputs
-        if self.opt['train']['gan_type'] == 'ciplab':
-            pass # TODO?
-        else:
-            self.log_dict['D_real'] += torch.mean(pred_d_real.detach()).item() / bm
-            self.log_dict['D_fake'] += torch.mean(pred_d_fake.detach()).item() / bm
-
         if use_amp:  # Use AMP stepper
             if self.cri_gan:
                 if step % self.D_update_ratio == 0 and step > self.D_init_iters:
